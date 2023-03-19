cb0ef451c79da838963e5e1b28788b3d24e1a58e
neokennyc <kenechukwuneolla@gmail.com>
2023-03-18 19:10:42 -0700

    success

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,11 @@
 #include <stdio.h>
 /*betty style doc for function main goes there */
 /**
- * main - main function
+ * main - Entry point
  *
- * Return always 0
+ * Description: "the program description"
+ *
+ * Return: always 0 (Success)
  */
 
 int main(void)
