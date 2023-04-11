d768cf4cc73edbb26bc10d3394885360cf367368
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-04-11 23:05:07 +0100

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicate using malloc
+ * @str: string  dub
+ * Return: Pointer to a the new duped string
+ */
+
+char *_strdup(char *s)
+{
+	char *a;
+	int i, c;
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0; c < i; c++)
+		a[c] = s[c];
+	a[c] = '\0';
+
+	return (a);
+}
