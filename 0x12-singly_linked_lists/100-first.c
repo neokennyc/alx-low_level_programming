92a53a838ea6dbb966cfae65344b6900d1cdd8eb
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-04-28 00:49:15 +0100

    00-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * hmain - function executed before main
+ * Return: no return.
+ */
+void __attribute__((constructor)) hmain()
+{
+    printf("You're beat! and yet, you must allow");
+    printf(",\nI bore my house upon my back!\n");
+}
