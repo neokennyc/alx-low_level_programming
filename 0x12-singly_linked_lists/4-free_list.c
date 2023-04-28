2ba2d6307289f3dcdbf226bf46d51a65b67debec
neokennyc <kenechukwuneolla@gmail.com>
2023-04-27 17:15:28 -0700

    betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,11 +6,12 @@
  */
 void free_list(list_t *head)
 {
-    list_t *current;
-    while ((current = head) != NULL)
-    {
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
