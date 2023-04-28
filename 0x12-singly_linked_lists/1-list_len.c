c440f9608eedcb12017e659b6fba89b7fdec21a8
neokennyc <kenechukwuneolla@gmail.com>
2023-04-27 17:10:09 -0700

    betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,12 +6,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    size_t n;
-    n = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        n++;
-    }
-    return (n);
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
