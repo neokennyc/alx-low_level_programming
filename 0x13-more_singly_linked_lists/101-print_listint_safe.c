b485aea4816af4a74cfe8862484da58bf8ffa9d6
neokennyc <kenechukwuneolla@gmail.com>
2023-04-29 09:07:34 -0700

    print£

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,41 @@
 #include "lists.h"
-/**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
- *
- * Return: number of nodes in the list
- */
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+    const listint_t *visited[1024] = {NULL};
+    size_t count = 0, i;
 
-	while (head)
-	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-	}
-	return (num);
+    while (head != NULL)
+    {
+        for (i = 0; i < count; i++)
+        {
+            if (head == visited[i])
+            {
+                printf("-> [%p] %d\n", (void *)head, head->n);
+                return (count);
+            }
+        }
+
+        visited[count++] = head;
+        printf("[%p] %d\n", (void *)head, head->n);
+        head = head->next;
+
+        if (count >= 1024)
+        {
+           
+            exit(98);
+        }
+    }
+
+    return (count);
 }
+
