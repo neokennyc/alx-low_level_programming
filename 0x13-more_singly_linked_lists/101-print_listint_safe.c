38ed629b2170681befb92a521ffb94c498298172
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-04-29 16:33:10 +0100

    1-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,7 +9,11 @@ size_t print_listint_safe(const listint_t *head)
 {
     size_t num = 0;
     long int diff;
-  
+    
+  if (head == NULL)
+{
+		 exit(98);
+     } 
     while (head)
     {
         diff = head - head->next;
