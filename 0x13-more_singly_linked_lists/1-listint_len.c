66c48063f95802ad1d0e62b6ab49e50626c4ca7a
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-04-30 04:33:01 +0100

    -listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,19 @@
 
+#include "lists.h"
+/**
+ * listint_len - Calculate size of elements.
+ * @head: head node
+ * Return: Int
+ **/
+size_t listint_len(const listint_t *head)
+{
+	const listint_t *tp;
+	unsigned int cnr = 0;
+	tp = head;
+	while (tp)
+	{
+		cnr++;
+		tp = tp->next;
+	}
+	return (cnr);
+}
