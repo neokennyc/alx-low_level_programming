0422a0a1edc568a14bb5e7b13850741963235945
neokennyc <kenechukwuneolla@gmail.com>
2023-04-29 22:11:06 -0700

    findloop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,9 +2,8 @@
 
 /**
  * find_listint_loop - Finds the loop in a linked list
- * @head: Pointer to the head of the linked list
- *
- * Return: The address of the node where the loop starts, or NULL if there is no loop
+ * @head: Pointer to the head
+ * Return: The address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
