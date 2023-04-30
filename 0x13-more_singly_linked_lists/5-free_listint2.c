80aaeb2f99dfa2d8fc3f1344a382b0a58a86d8ee
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-04-30 05:47:48 +0100

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - =======
+ * @head: pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *curr;
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		 = (*head)->next;
+		free(*head);
+		*head = curr;
+	}
+	*head = NULL;
+}
