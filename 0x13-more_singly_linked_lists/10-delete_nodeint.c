85c01ec5f5a0a65538e01a56d115dc6e1fa31adf
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-04-30 18:45:27 +0100

    10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node 
+ * @head: =======
+ * @index: =======
+ *
+ * Return: 1 (Success), or -1 (Fail)
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *curr = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
+  
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(curr);
+		return (1);
+	}
+	while (i < index - 1)
+	{
+		if (!curr || !(curr->next))
+			return (-1);
+		 curr = curr->next;
+		i++;
+	}
+	current = curr->next;
+	
+  curr->next = current->next;
+	free(current);
+	return (1);
+}
