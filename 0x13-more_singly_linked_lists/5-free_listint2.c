2c4c916fe3e43cb45cba52c36a58d9242bacd905
neokennyc <kenechukwuneolla@gmail.com>
2023-04-29 22:00:24 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,11 +6,12 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *curr;
+
 	if (head == NULL)
 		return;
 	while (*head)
 	{
-		 curr = (*head)->next;
+		curr = (*head)->next;
 		free(*head);
 		*head = curr;
 	}
