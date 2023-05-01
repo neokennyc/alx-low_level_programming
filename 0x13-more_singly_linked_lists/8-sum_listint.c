79ad28ad453bb2a34cfd00ca57eb48285af68394
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-05-01 17:18:03 +0100

    Create 8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * sum_listint - sum all nodes
+ * @head: pointer to head node
+ * Return: N
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int n = 0;
+	if (!(head))
+		return (0);
+	while (head)
+	{
+		n = n + head->n;
+		head = head->next;
+	}
+	return (n);
+}
