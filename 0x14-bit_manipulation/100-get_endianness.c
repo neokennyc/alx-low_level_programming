a0c3dc308a824e2791cf9bfa693ca84727784fa1
kenechukwu Muoneme <112420519+neokennyc@users.noreply.github.com>
2023-05-04 23:57:34 +0100

    hhh

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "main.h"
+/**
+ *  get_endianness - checks for endianess
+ *
+ *  Return: void
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+  
+	char *c = (char *) &x;
+	return (*c);
+}
